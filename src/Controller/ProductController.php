e03ce6a179859421ad5b3b689b13c1b029bbd1ca
loris <loris.ec@outlook.com>
2020-10-06 18:33:13 +0200

    add product CRUD

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use JMS\Serializer\SerializerInterface;
+use FOS\RestBundle\Controller\ControllerTrait;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class ProductController extends AbstractController
 {
+    use ControllerTrait;
+
     /**
      * @Route("/products", name="list_product", methods={"GET"})
      */
@@ -27,19 +33,64 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/products/{id}", name="show_product")
+     * @Rest\Get(
+     *     path = "/products/{id}",
+     *     name = "show_product",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @Rest\View(StatusCode = 200)
      */
-    public function showProduct(SerializerInterface $serialize)
+    public function showProduct(Product $product)
     {
-        $product = new Product();
-        $product
-            ->setName('article')
-            ->setDescription('Le contenu de mon article.');
-        $data = $serialize->serialize($product, 'json');
+        return $product;
+    }
 
-        $response = new Response($data);
-        $response->headers->set('Content-Type', 'application/json');
+    /**
+     * @Rest\Post("/products", name="create_product")
+     * @Rest\View(StatusCode = 201)
+     * @ParamConverter("product", converter="fos_rest.request_body")
+     */
+    public function createProduct(Product $product)
+    {
+        $manager = $this->getDoctrine()->getManager();
 
-        return $response;
+        $manager->persist($product);
+        $manager->flush();
+
+        return $this->view($product, Response::HTTP_CREATED, ['Location' => $this->generateUrl('show_product', ['id' => $product->getId(), UrlGeneratorInterface::ABSOLUTE_URL])]);
+    }
+
+    /**
+     * @Rest\Put("/products", name="edit_product")
+     * @Rest\View(StatusCode = 200)
+     * @ParamConverter("product", converter="fos_rest.request_body")
+     */
+    public function editProduct(Product $product)
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        $manager->persist($product);
+        $manager->flush();
+
+        return $this->view($product, Response::HTTP_OK, ['Location' => $this->generateUrl('show_product', ['id' => $product->getId(), UrlGeneratorInterface::ABSOLUTE_URL])]);
+    }
+
+    /**
+     * @Rest\Delete(
+     *  path = "/products/{id}",
+     *  name = "delete_product",
+     *  requirements = {"id"="\d+"}
+     * )
+     * @Rest\View(StatusCode = 204)
+     * @ParamConverter("product", converter="fos_rest.request_body")
+     */
+    public function deleteProduct(Product $product)
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        $manager->remove($product);
+        $manager->flush();
+
+        return $this->view('', Response::HTTP_NO_CONTENT);
     }
 }
